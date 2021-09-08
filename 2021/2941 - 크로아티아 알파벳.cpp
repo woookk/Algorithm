c2f2760e8d0c99bc7f1cc7d810e237d98ca34fe2
woookk <woook27@naver.com>
2021-09-09 00:18:17 +0900

    string

---
+++ 2021/2941 - 크로아티아 알파벳.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+int main() {
+
+	string word;
+	getline(cin, word);
+	int cnt = word.length();
+	for (int i = 0; i < word.length(); i++) {
+		if (word[i] == 'd' && word[i + 1] == 'z' && word[i+2]=='=') cnt -= 1;
+		else if (word[i] == 'l' && word[i + 1] == 'j') cnt -= 1;
+		else if (word[i] == 'n' && word[i + 1] == 'j') cnt -= 1;
+		else if (word[i] == '-' || word[i] == '=') cnt -= 1;
+	}
+	cout << cnt;
+	return 0;
+}
