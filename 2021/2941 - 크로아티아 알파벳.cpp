469285689d979e661cb7ad3e39be68cda4b1e318
woookk <woook27@naver.com>
2021-09-09 22:09:34 +0900

    string

--- 2021/2941 - 크로아티아 알파벳.cpp
+++ 2021/2941 - 크로아티아 알파벳.cpp
@@ -9,8 +9,7 @@ int main() {
 	int cnt = word.length();
 	for (int i = 0; i < word.length(); i++) {
 		if (word[i] == 'd' && word[i + 1] == 'z' && word[i+2]=='=') cnt -= 1;
-		else if (word[i] == 'l' && word[i + 1] == 'j') cnt -= 1;
-		else if (word[i] == 'n' && word[i + 1] == 'j') cnt -= 1;
+		else if ((word[i] == 'l' || word[i] == 'n')&& word[i + 1] == 'j') cnt -= 1;
 		else if (word[i] == '-' || word[i] == '=') cnt -= 1;
 	}
 	cout << cnt;
