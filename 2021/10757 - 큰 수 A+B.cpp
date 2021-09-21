23c7eeb5a03936f4f107d28137d8a6224b234539
woookk <woook27@naver.com>
2021-09-21 13:48:44 +0900

    math

---
+++ 2021/10757 - 큰 수 A+B.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<string>
+using namespace std;
+string A, B, C, sum;
+int up = 0;
+string reverse(string A) {
+	string a=A;
+	for (int i = 0; i < A.length(); i++)
+		a[i]= A[A.length() - 1 - i];
+	return a;
+}
+string add(string A, string B) {
+	string sum;
+	int x = 0, y = 0;
+	for (int i = 0; i < B.length(); i++) {
+		x = A[i] - '0'; y = B[i] - '0';
+		sum += to_string((x + y + up) % 10);
+		up = 0;
+		if (x + y +up>= 10) {
+			up++;
+		}
+	}
+	for (int i = B.length(); i < A.length(); i++) {
+		x = A[i] - '0';
+		sum += to_string((x + up)%10);
+		up = 0;
+		if (x + up >= 10) {
+			up = 1;
+		}
+	}
+	return sum;
+}
+int main() {
+	cin >> A >> B;
+	if (A.length() < B.length()) {
+		C = A;
+		A = B;
+		B = C;
+	}
+	A=reverse(A); B=reverse(B);
+	sum= add(A, B);
+	if (up)
+		sum += '1';
+	cout << reverse(sum);
+	return 0;
+}
