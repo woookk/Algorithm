0cb86d0193b73933079d1405da26d5eb07d3e956
woookk <woook27@naver.com>
2021-11-20 02:17:06 +0900

    0x03 - String

--- basic-algo/10808 - 알파벳 개수.cpp
+++ basic-algo/10808 - 알파벳 개수.cpp
@@ -4,14 +4,23 @@ using namespace std;
 
 int main() {
 
-	string word;
-	cin >> word;
+	string s;
+	cin >> s;
 	int A[26] = { 0 };
-	for (int i = 0; i < word.length(); i++) {
-		A[(int)word[i] - 97]++;
+	for (int i = 0; i < s.length(); i++) {
+		A[(int)s[i] - 97]++; // A[s[i] - 'a']로 표현 가능.
 	}
 	for (int e : A){
 		cout << e << " ";
 	}
 	return 0;
 }
+
+/* 
+for (char a = 'a'; a <= 'z'; a++) {
+	int cnt = 0;
+	for(auto c : s)
+		if(a == c) cnt++;
+		cout << cnt << ' '
+	이런 풀이도 가능.
+*/
