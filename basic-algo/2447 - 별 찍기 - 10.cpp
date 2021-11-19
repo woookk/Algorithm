a2c3a82b46b7ba066054e321af1055798796c05b
woookk <woook27@naver.com>
2021-11-19 16:14:55 +0900

    0x02 - Implementation

---
+++ basic-algo/2447 - 별 찍기 - 10.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+char star[6562][6562];
+int N;
+void recur() { 
+	if (N == 3) {
+
+	}
+	for (int i = 0; i < N; i++) {
+
+	}
+}
+int main() {
+	cin >> N;
+	return 0;
+}
