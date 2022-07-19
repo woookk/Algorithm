41c943f20b4125256a8060809b112dcd1593cf3e
민병욱 <woook27@naver.com>
2022-07-20 01:37:57 +0900

    Two-pointer

--- BOJ/2531 - 회전 초밥.cpp
+++ BOJ/2531 - 회전 초밥.cpp
@@ -11,41 +11,34 @@ using namespace std;
 #define FASTIO ios::sync_with_stdio(0); cin.tie(0);
 
 int N, d, k, c;
-int dishes[30001 * 2];
-int ateCount[3001];
-int ateKindCount;
+int a[30001];
+int kind[3001];
 int ans;
 
-void eat(int sushi) {
-  if (ateCount[sushi] == 0) {
-    ateKindCount++;
-    ans = max(ans, ateKindCount);
-  }
-  ateCount[sushi]++;
-}
-
-void overeat(int sushi) {
-  ateCount[sushi]--;
-  if (ateCount[sushi] == 0) ateKindCount--;
-}
-
 int main() {
-  FASTIO
-  cin >> N >> d >> k >> c;
-  ateCount[c]++;
-  ateKindCount = 1;
-  ans = 1;
-
-  for (int i = 0; i < N; ++i) {
-    cin >> dishes[i];
-    dishes[N + i] = dishes[i];
-  }
-
-  for (int i = 0; i < N * 2; ++i) {
-    if (i >= k) overeat(dishes[i - k]);
-    eat(dishes[i]);
-  }
-  cout << ans;
-
-  return 0;
+    FASTIO
+    cin >> N >> d >> k >> c;
+    for (int i = 0; i < N; ++i) {
+        cin >> a[i];
+    }
+    int tot = 0;
+    for(int i = N - k; i < N; i++){
+        if(kind[a[i]] == 0)
+            tot++;
+        kind[a[i]]++;
+    }
+    for(int i = 0; i < N; i++){
+        if(kind[a[i]] == 0)
+            tot++;
+        kind[a[i]]++;
+        int st = i - k;
+        if(st < 0)
+            st += N;
+        if(kind[a[st]] == 1)
+            tot--;
+        kind[a[st]]--;
+        ans = max(ans, tot + (kind[c] == 0));
+    }
+    cout << ans;
+    return 0;
 }
